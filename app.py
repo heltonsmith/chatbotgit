ac7718daf0a732ed333e0f181db2aa18b5091d3e
Helton <heltonsmith@hotmail.com>
2021-07-24 02:56:06 -0400

    asdas

--- app.py
+++ app.py
@@ -1,69 +1,37 @@
-from chatterbot import ChatBot, conversation
-from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
+#!/usr/bin/env python
+"""A simple API to do almost nothing"""
 import hug
 
-####
-#!/usr/bin/env python3
-
-####class CORSRequestHandler (SimpleHTTPRequestHandler):
-####    def end_headers (self):
-####        self.send_header('Access-Control-Allow-Origin', '*')
-####        SimpleHTTPRequestHandler.end_headers(self)
-
-####if __name__ == '__main__':
-####    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
-####
-
-#se graba en log todo
-import logging
-logging.basicConfig(filename='archivolog.log', level=logging.DEBUG)
-#se graba en log todo
-
-#objeto de chatterbot
-chatbot = ChatBot('WinChat')
-
-#para borrar cache de aprendizaje
-chatbot.storage.drop() #para borrar cache de aprendizaje
-
-#####añadido de listas
-entrenador = ListTrainer(chatbot)
-conversacionL = ["Holanda", "Hola Humano"] #para entrenar con un array
-entrenador.train(conversacionL)
-#####añadido de listas
-
-#url YML C:\Users\helto\AppData\Local\Programs\Python\Python39\Lib\site-packages\chatterbot_corpus\data
-
-#entrena con archivos YML
-entrenador = ChatterBotCorpusTrainer(chatbot)
-entrenador.train("./data/roca.yml")
-#entrenador.train(
-#    "./data/propio.json"
-#)
-#entrena con archivos YML
-
-#http://localhost:8000/respuestapi?pregunta=holanda&id=1
-#hug -f .\app.py
-
-@hug.response_middleware()
-def process_data(request, response, resource):
-      response.set_header('Access-Control-Allow-Origin', '*')
+users = [
+    {
+        'uid': u'jsmith',
+        'name': u'John Smith',
+        'email': u'jsmith@example.com',
+        'phone': u'123-456-7890'
+    },
+    {
+        'uid': u'jdoe',
+        'name': u'Jane Doe',
+        'email': u'jdoe@example.com',
+        'phone': u'234-567-8901'
+    },
+    {
+        'uid': u'ssample',
+        'name': u'Sally Sample',
+        'email': u'ssample@example.com',
+        'phone': u'345-678-9012'
+    }
+]
 
 @hug.get('/users', versions=1)
 def user(user_id):
     return 'I do nothing useful.'
     #return user_id
 
-@hug.get(examples='respuestapi?pregunta=holanda&id=1')
 @hug.local()
-def respuestapi(pregunta: hug.types.text, id: hug.types.number, hug_timer=3):
-    respuesta = chatbot.get_response(pregunta)
-    rrr=str(respuesta)
-    return  {
-                'id': id,
-                'pregunta': pregunta,
-                'respuesta': rrr,
-                'took': float(hug_timer)
-            }
+@hug.get('/users', versions=2)
+def user():
+    return {'users': users}
 
 if __name__ == '__main__':
     user.interface.local()
