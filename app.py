49eaa7135584e0bc1b674e3574efba2c37f6aace
Helton <heltonsmith@hotmail.com>
2021-07-24 00:52:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ import hug
 from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 import sys
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        SimpleHTTPRequestHandler.end_headers(self)
+####class CORSRequestHandler (SimpleHTTPRequestHandler):
+####    def end_headers (self):
+####        self.send_header('Access-Control-Allow-Origin', '*')
+####        SimpleHTTPRequestHandler.end_headers(self)
 
-if __name__ == '__main__':
-    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
+####if __name__ == '__main__':
+####    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
 ####
 
 #se graba en log todo
